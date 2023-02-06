6830504b425b16a00838e1469b31ec7eee5f59b5
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-06 18:16:32 +0530

    Separated the view with the model and used view model for maintaining the configuration changes.

--- app/src/main/java/com/view/model/app/MainActivity.kt
+++ app/src/main/java/com/view/model/app/MainActivity.kt
@@ -4,22 +4,40 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    var count = 0
+
     private lateinit var tvCount: TextView
     private lateinit var btnIncrement: Button
+
+    private lateinit var mainViewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         initView()
+        setText()
 
     }
 
     private fun initView() {
         tvCount = findViewById(R.id.tvCount)
         btnIncrement = findViewById(R.id.btnIncrement)
+
+        btnIncrement.setOnClickListener {
+            increment()
+        }
+    }
+
+    private fun increment() {
+        mainViewModel.increment()
+        setText()
+    }
+
+    private fun setText() {
+        tvCount.text = mainViewModel.count.toString()
     }
 }
--- app/src/main/java/com/view/model/app/MainViewModel.kt
+++ app/src/main/java/com/view/model/app/MainViewModel.kt
@@ -3,4 +3,9 @@ package com.view.model.app
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    var count : Int = 0
+
+    fun increment(){
+        count++
+    }
 }
